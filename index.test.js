b8d006f3eec7d9566d163e855110926018b38256
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-02-12 21:51:17 +0700

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -3,12 +3,14 @@ const goblox = require('./lib/main.js')
 async function test() {
     const account = await new goblox.loginCookie("_|WARNING:-DO-NOT-SHARE-THIS...").catch(console.error);
 
-    console.log(account) //undefined because cookie is invalid
+    console.log(account) //undefined
     goblox.getAccountInfo("fastering18").then(re=>{
         console.log(JSON.stringify(re))
         goblox.getStatus(re.UserId).then(r=>{
             console.log(JSON.stringify(r))
         });
     });
+    
+    goblox.devforum.getUser(467971019).then(console.log).catch(console.log)
 }
 test()
